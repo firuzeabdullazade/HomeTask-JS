837089406c60c68164b72dc7490c4f9ca62563a7
firuzeabdullazade <firuzeabdullazade64@gmail.com>
2023-04-16 15:38:22 +0400

    Task 10 was recorrected

--- task10.js
+++ task10.js
@@ -3,11 +3,12 @@ function bankDeposit() {
     const initialAmount = +prompt('Enter the initial amount')
     const durationYear = +prompt('Enter the duration years')
     const percentForYear = +prompt('Enter the % per year')
+    let amount = 0
     
     for (let i = 1; i <= durationYear; i++) {
-     const amount = initialAmount*(1+(percentForYear/100))**i;
-     return `${amount}`
+     amount = initialAmount*((1+(percentForYear/100))**i);
     }
+    return `${amount}`
 } 
 
-bankDeposit()
+console.log(bankDeposit())
